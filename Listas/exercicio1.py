6d0f0811e5cc683a4dfd0b49df47861801f338fc
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-02-10 11:25:16 -0300

    Add files via upload

---
+++ Listas/exercicio1.py
@@ -0,0 +1,5 @@
+lista_numeros = []
+for i in range(5):
+    valor = int(input("Digite um número:\n"))
+    lista_numeros.append(valor)
+print(lista_numeros)
---
+++ Listas/exercicio2.py
@@ -0,0 +1,6 @@
+lista_numeros = []
+for i in range(10):
+    numero = int(input("Digite um número para adicionar na lista:\n "))
+    lista_numeros.append(numero)
+    lista_numeros.reverse()
+    print(lista_numeros)
